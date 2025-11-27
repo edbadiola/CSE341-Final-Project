5db684bfa4d261a1cce67088dbaa579544e72e09
edbadiola <u000681426@byui.edu>
2025-11-27 23:17:00 +0800

    changes

--- server.js
+++ server.js
@@ -45,15 +45,14 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.CALLBACK_URL,
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
     },
     function (accessToken, refreshToken, profile, done) {
-      //User.findOrCreate({ githubId: profile.id }, function (err, user) {
       return done(null, profile);
-      //});
     }
   )
 );
+
 
 passport.serializeUser((user, done) => {
   done(null, user);
